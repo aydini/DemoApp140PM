3ba68900a22b4290971eb2fe0f2b000c1eaa7c42
ilknur <ilknur@137.125.155.32>
2022-05-10 14:46:11 -0400

    first commmit

---
+++ src/main/java/aydin/demoapp140pm/DemoApp140PM.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package aydin.demoapp140pm;
+
+/**
+ *
+ * @author ilknur
+ */
+public class DemoApp140PM {
+
+    public static void main(String[] args) {
+        System.out.println("Hi! This is a computer game with two players...");
+        System.out.println("Creating two players...");
+        Player p1 = new Player("Tony", 0);
+        Player p2 = new Player("Ramses", 0);
+        System.out.println("Created Players...");
+        
+    }
+}
---
+++ src/main/java/aydin/demoapp140pm/Player.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aydin.demoapp140pm;
+
+/**
+ *
+ * @author ilknur
+ */
+public class Player {
+    // fields of the class Player
+    private String name;
+    private int score;
+    
+    // constructor with parameters
+    public Player(String theName, int theScore ){
+        name = theName;
+        score = theScore;
+    }
+    
+}
